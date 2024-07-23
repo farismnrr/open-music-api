78712a51cc005e4030a62658dcbe1fe73095e210
farismnrr <farismunir2@gmail.com>
2024-07-23 22:29:57 +0700

    Update some bugs

--- src/services/postgres/AlbumService.js
+++ src/services/postgres/AlbumService.js
@@ -12,11 +12,10 @@ class AlbumService {
 	async addAlbum({ name, year }) {
 		const id = nanoid();
 		const createdAt = new Date().toISOString();
-		const updatedAt = createdAt;
 
 		const query = {
-			text: "INSERT INTO albums (id, name, year, created_at, updated_at) VALUES($1, $2, $3, $4, $5) RETURNING id",
-			values: [id, name, year, createdAt, updatedAt]
+			text: "INSERT INTO albums VALUES($1, $2, $3, $4, $4) RETURNING id",
+			values: [id, name, year, createdAt]
 		};
 
 		const result = await this._pool.query(query);
