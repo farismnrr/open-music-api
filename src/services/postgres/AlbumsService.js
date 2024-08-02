f2d59eed16f016d1d539eb48849db1d24b5da77a
farismnrr <farismunir2@gmail.com>
2024-08-03 03:05:31 +0700

    refactoring auth and album service

--- src/services/postgres/AlbumsService.js
+++ src/services/postgres/AlbumsService.js
@@ -11,19 +11,17 @@ class AlbumsService {
 
 	async addAlbum({ name, year }) {
 		const id = `album-${nanoid()}`;
-
-		const query = {
+		const albumQuery = {
 			text: "INSERT INTO albums VALUES($1, $2, $3, $4) RETURNING id",
 			values: [id, name, year, null]
 		};
 
-		const result = await this._pool.query(query);
-
-		if (!result.rowCount) {
+		const albumResult = await this._pool.query(albumQuery);
+		if (!albumResult.rowCount) {
 			throw new InvariantError("Gagal menambahkan album.");
 		}
 
-		return mapAlbumsModel(result.rows[0]).id;
+		return mapAlbumsModel(albumResult.rows[0]).id;
 	}
 
 	async getAlbumById(id) {
@@ -37,53 +35,53 @@ class AlbumsService {
 			throw new NotFoundError("Album tidak ditemukan.");
 		}
 
-		const album = mapAlbumsModel(albumResult.rows[0]);
+		const albums = mapAlbumsModel(albumResult.rows[0]);
 		const songQuery = {
 			text: "SELECT id, title, performer FROM songs WHERE album_id=$1",
-			values: [album.id]
+			values: [albums.id]
 		};
 
 		const songResult = await this._pool.query(songQuery);
 		const songs = songResult.rows.map(mapSongsModel);
 
 		return {
-			...album,
+			...albums,
 			songs
 		};
 	}
 
 	async editAlbumById(id, { name, year }) {
-		const query = {
+		const albumQuery = {
 			text: "UPDATE albums SET name=$1, year=$2, cover=$3 WHERE id=$4 RETURNING id",
 			values: [name, year, null, id]
 		};
 
-		const result = await this._pool.query(query);
-		if (!result.rowCount) {
+		const albumResult = await this._pool.query(albumQuery);
+		if (!albumResult.rowCount) {
 			throw new NotFoundError("Gagal memperbarui album.");
 		}
 	}
 
 	async deleteAlbumById(id) {
-		const query = {
+		const albumQuery = {
 			text: "DELETE FROM albums WHERE id=$1 RETURNING id",
 			values: [id]
 		};
 
-		const result = await this._pool.query(query);
-		if (!result.rowCount) {
+		const albumResult = await this._pool.query(albumQuery);
+		if (!albumResult.rowCount) {
 			throw new NotFoundError("Gagal menghapus album.");
 		}
 	}
 
 	async updateAlbumCoverUrl(id, url) {
-		const query = {
+		const albumQuery = {
 			text: "UPDATE albums SET cover = $2 WHERE id = $1",
 			values: [id, url]
 		};
 
-		const result = await this._pool.query(query);
-		if (!result.rowCount) {
+		const albumResult = await this._pool.query(albumQuery);
+		if (!albumResult.rowCount) {
 			throw new NotFoundError("Gagal menambahkan cover album.");
 		}
 	}
